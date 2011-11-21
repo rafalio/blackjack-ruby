c9933733395fb2a2b445386d1605807162361917
Rafal Szymanski <me@rafal.io>
2011-11-21 23:42:03 +0000

    I think I didn't understand blackjack at first.... Now the value of an ace is 11, unless it makes the pack value go over 21
    Did everything except splits... Might do this if I have time, but I have
    to go do the following courseworks - Network Security and Complex
    Systems....

--- blackjack.rb
+++ blackjack.rb
@@ -1,46 +1,22 @@
 require 'pp'
 
-CARDS = [["2",2],["3",3],["4",4],["5",5],["6",6],["7",7],["8",8],["9",9],["10",10],["J",10],["K",10],["Q",10],["A",0]]
+CARDS = [2,3,4,5,6,7,8,9,10,"J","K","Q","A"]
 
 class Player
-  attr_accessor :cards, :money, :bet
-  def initialize(cards,money)
+  attr_accessor :cards, :money, :bet, :cur_playing, :index
+  def initialize(cards,money,index)
     @cards = cards
     @money = money
     @bet = 0
+    @cur_playing = true  # Indicates whether the player is currently in-game
+    @index = index
+  end
+
+  def reset()
+    @cards = Array.new
+    @bet = 0
     @cur_playing = true
   end
-
-  def lost()
-    value() > 21
-  end
-
-  def finished()
-    @cur_playing = false
-  end
-  
-  def lose_money()
-    @money -= @bet
-  end
-
-  def blackjack_money()
-    @money += @bet*1.5
-  end
-
-  def win_money
-    @money += @bet
-  end
-
-  #calculate value of given hand
-  def value()
-    @cards.map{|c| c[1]}.inject{|r,cur| r + cur}
-  end
-
-  def blackjack()
-    value() == 21 && @cards.length == 2
-  end
-
-
 
 end
 
@@ -50,39 +26,98 @@ class Blackjack
   def initialize()
     @cards = Array.new()
     @players = Array.new()
-    @dealer = Array.new()
-    setup_game()
-  end
-
-  def setup_game()
-    #puts "How many players at the table?"
-    #@num_players = gets.to_i
-    #puts "How many decks of cards are we playing with?"
-    #@num_decks = gets.to_i
-    @num_players = 4
-    @num_decks = 4
-    (@num_decks*4).times { @cards += CARDS }
-    @cards = @cards.sort_by { rand }
-
-    @num_players.times do |i|
-      two_cards = [@cards.pop, @cards.pop]
-      p = Player.new(two_cards, 1000)
-      @players.push p
-    end
-
-    2.times { @dealer.push @cards.pop }
-
-    get_player_bets()
-    player_get_ace_values()
-    print_state()
+    @dealer = Array.new() # Dealer's cards
     game_loop()
   end
 
+  # Calculates the value of a hand
+  # If Ace is present, assume 11 unless it makes the hand go over 21
+  # I think that's how it works in Blackjack
+  def value(hand)
+    # Sorting hack to get aces at the very end so we count them last
+    hand.sort_by { |c| c.to_i != 0 ? c : c[0] - 81 }.reverse().inject(0) do |total,cur|
+      if cur.to_i != 0
+        total + cur # 2-10 case
+      elsif ["J","Q","K"].include? cur
+        total + 10 # J,Q, or K
+      elsif cur == "A"
+        if (total+11) > 21
+          total + 1 # Count ace as 1
+        else
+          total+11 # Count ace as 11
+        end
+      end
+    end
+  end
+
+  # Returns true if the hand is a blackjack
+  def blackjack(hand)
+    value(hand) == 21 && hand.length == 2
+  end
+
+  # Credits the player if he wins the round
+  def credit_player(p, blackjack = false)
+    if blackjack
+      p.money += p.bet*1.5
+    else
+      p.money += p.bet
+    end
+  end
+
+  # Player loses his bet if he lost
+  def debit_player(p)
+    p.money -= p.bet
+  end
+
+  # Sets up one game...
+  def setup_game()
+    @players.each { |p| p.reset() } # Reset the player
+    @cards = Array.new # Reset cards 
+    @dealer = Array.new # Reset dealer cards
+    (@num_decks*4).times { @cards += CARDS }
+    puts "Shuffling cards...."
+    @cards = @cards.sort_by { rand } # Hack to shuffle. Ruby 1.9 has shuffling built-in!
+
+    @players.find_all { |p| p.money <= 0}.each do |p|
+      puts "Player #{p.index} is out of money, removing from game..."
+    end
+    @players.reject! { |p| p.money <= 0}
+    if @players.length == 0
+      puts "Everybody is out of money, quitting game..."
+      exit()
+    end
+
+
+    @players.each do |p|
+      p.cards = [@cards.pop, @cards.pop]
+    end
+
+    puts "Dealer taking cards...."
+    2.times { @dealer.push @cards.pop }
+
+    get_player_bets()
+  end
+
+
+  def table_setup()
+    puts "How many players at the table?"
+    @num_players = gets.to_i
+    puts "How many decks of cards are we playing with?"
+    @num_decks = gets.to_i
+
+    # Initialize the players....
+    @num_players.times do |i|
+      p = Player.new(Array.new, 1000, i)
+      @players.push p
+    end
+
+  end
+
   def get_player_bets()
-    @players.each_with_index do |p,i|
+    @players.each do |p|
       done = false
       while(!done)
-        print "Player #{i}, please enter your bet for this round: "
+        print "Player #{p.index}, please enter your bet for this round: "
         bet = gets.to_i
         if(bet > 0 && bet <= p.money)
           p.bet = bet
@@ -92,43 +127,18 @@ class Blackjack
     end
   end
 
-  # Handling of aces, player has to decide whether he wants ace to be 1 or 11
-  # This is pretty bad code, but I need to go and do coursework....
-  def player_get_ace_values()
-    puts "calling get ace values"
-    @players.each_with_index do |p,i|
-      p.cards.each_with_index do |c,i2|
-        if c[1] == 0
-          done = false
-          while(!done)
-            puts "Player #{i}, you have an ace. Please fix its value to either 1 or 11: "
-            puts "Your current hand is #{p.cards.map{ |c| "(#{c[0]}, value: #{c[1]})"}.join(',')}"
-            print "1 or 11: "
-            v = gets.to_i
-            if (v == 1 || v == 11)
-              p.cards[i2][1] = v
-              done = true
-            end
-          end
-        end
-      end
-    end
-  end
-
-
   def print_state()
     puts "------- CURRENT BOARD STATE ---------"
-    puts "DEALER: #{@dealer.join(',')} \n\n"
+    puts "DEALER: #{@dealer[0]}, X \n\n"
 
-    @players.each_with_index do |p,i|
-      puts "PLAYER #{i}"
+    @players.each do |p|
+      puts "PLAYER #{p.index} \t #{p.cur_playing ? "(in game)" : "(lost)"}"
       puts "\t money: \t #{p.money}"
       puts "\t bet: \t\t #{p.bet}"
-      puts "\t cards: \t #{p.cards.map{ |c| "(#{c[0]}, value: #{c[1]})"}.join(',')}"
+      puts "\t cards: \t #{p.cards.join(',')}"
+      puts "\t value: \t #{value p.cards}"
     end
     puts "\n------- END CURRENT BOARD STATE ---------"
-
-    pp @cards
   end
 
 
@@ -136,61 +146,118 @@ class Blackjack
     puts "available commands for a player: split, double, stand, hit"
   end
 
+  def dealer_reveal_cards()
+    puts "Delear reveals his second card...."
+    puts "Dealers cards are #{@dealer.join(',')}"
 
-  def game_loop()
-    cur_player = 0
-
-    # Initial loop to go through players and get their choices
-    while(cur_player < @num_players)
-      pl = @players[cur_player]
-      player_done = false
-
-      while (!player_done)
-        print_state()
-        print "Player #{cur_player}, what would you like to do: "
-        action = gets.chomp
-        case action
-
-        when "hit"
-          new_card = @cards.pop 
-          pl.cards << new_card
-          player_get_ace_values if new_card[0] == "A"
-
-        when "split"
-          puts "not implemented"
-
-        when "double"
-          new_card = @cards.pop
-          pl.cards.push new_card
-          player_get_ace_values if new_card[0] == "A"
-          player_done = true
-
-        when "stand"
-          player_done = true
-
-        else
-          puts "no such action, try again..."
-        end
-
-        if pl.blackjack()
-          puts "Player #{cur_player} has a blackjack!"
-          pl.blackjack_money()
-          pl.finished()
-        elsif pl.lost()
-          puts "Player #{cur_player}, you lost..."
-          pl.lose_money()
-          pl.finished()
-        end
-        player_done = true
-      end
-        cur_player += 1
+    d = value(@dealer)
+    # Dealer hits on <= 16
+    while d < 17
+      puts "Dealer takes a new card..."
+      @dealer << @cards.pop
+      puts "Dealers' cards are #{@dealer.join(',')}"
+      d = value(@dealer)
     end
 
+    # By now,  17 <= value(@dealer) <= 26
+  end
+
+
+  # This runs after the initial round, and after the dealer reveals his cards
+  # We compare the hand of the dealer with every players hands, and act accordingly
+  def final_round()
+    d = value(@dealer)
+    puts "--- Check Round --- "
+    puts "Dealers' cards are #{@dealer.join(',')} for a value #{d}"
+
+    # Iterate over all players who are still in the game,
+    # as in they haven't lost in the initial round doing 'hits'
+    #
+    # Precondition: forall p in players where p.cur_playing == false, value(p.cards) <= 21
+    @players.find_all{|p| p.cur_playing}.each do |p|
+      if value(p.cards) < d && d <= 21 # Dealer Wins
+        puts "Player #{p.index} has deck worth #{value p.cards}, and loses to the dealer..."
+        debit_player(p)
+      elsif (value(p.cards) > d && d <= 21) || d > 21  # Player Wins
+        puts "Player #{p.index} has deck worth #{value p.cards}, and wins with the dealer..."
+        credit_player(p)
+      elsif value(p.cards) == d
+        puts "Player #{p.index} has deck worth #{value p.cards}, and draws with the dealer..."
+      end
+    end
+  end
+
+
+  # Main game loop
+  def game_loop()
+    table_setup() # Get num players and number of decks to use
+
+    while true
+      puts "----- STARTING A NEW GAME ------"
+      setup_game()    # Shuffle cards, etc...
+      one_game_loop() # Have one game
+    end 
 
   end
 
 
 
+  # Loop for one game of blackjack
+  def one_game_loop()
+
+    # Initial loop to go through players and get their choices
+    @players.each do |pl|
+      playing = true
+
+      while (playing)
+        print_state()
+
+        # Check if player has a blackjack at the very beginning
+        if blackjack(pl.cards)
+          puts "Player #{pl.index} has a blackjack!"
+          credit_player(pl,blackjack=true)
+          playing = false
+          pl.cur_playing = false    # Player has already won, don't include in final round
+          next
+        end
+
+        print "Player #{pl.index}, what would you like to do: "
+        action = gets.chomp
+
+        case action
+        when "hit"
+          new_card = @cards.pop 
+          pl.cards << new_card
+        when "split"
+          puts "not implemented, I have to go do homework!"
+        when "double"
+          new_card = @cards.pop
+          pl.cards << new_card
+          playing = false
+        when "stand"
+          playing = false
+        else
+          puts "no such action, try again..."
+        end
+
+        if value(pl.cards) > 21 # Lost :(
+          puts "Player #{pl.index}, you lost..."
+          debit_player(pl)
+          playing = false
+          pl.cur_playing = false    # Player has already lost, don't inlude in final round
+        elsif value(pl.cards) == 21 # 21, but not blackjack, so maybe we draw with the dealer, we check later
+          puts "Player #{pl.index} has 21 points - we await for the final round to compare with the dealer"
+          playing = false
+        end
+      end
+    end
+
+    # By this point, the first round is finished. Dealer starts revealing cards...
+    
+    dealer_reveal_cards()
+    final_round()
+
+  end
 end
 
 b = Blackjack.new()
